c7e084d4cf038ffd4340a1ab9b67f86e88fab4bc
Miserymon <diegosalvo87@gmail.com>
2023-10-29 22:21:17 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/webview/MainActivityTest.java
@@ -0,0 +1,37 @@
+package com.example.webview;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction linearLayout = onView(
+                allOf(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)),
+                        isDisplayed()));
+        linearLayout.check(matches(isDisplayed()));
+    }
+}
